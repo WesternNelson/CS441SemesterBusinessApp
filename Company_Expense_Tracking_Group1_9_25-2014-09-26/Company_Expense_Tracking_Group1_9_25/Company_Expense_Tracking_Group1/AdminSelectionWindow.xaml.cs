f6601765438c4e44aa010bcbabc13c873812cde1
WesternNelson <Western.nelson@gmail.com>
2014-09-26 13:38:25 -0700

    First Project upload commit

---
+++ Company_Expense_Tracking_Group1_9_25-2014-09-26/Company_Expense_Tracking_Group1_9_25/Company_Expense_Tracking_Group1/AdminSelectionWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Company_Expense_Tracking_Group1
+{
+    /// <summary>
+    /// Interaction logic for AdminSelectionWindow.xaml
+    /// </summary>
+    public partial class AdminSelectionWindow : Window
+    {
+        Handler adminSelectionHandler = new Handler();
+        public AdminSelectionWindow()
+        {
+            InitializeComponent();
+            Date_Time_Label.Content = adminSelectionHandler.currentDateTime();
+        }
+
+        /* 
+        *   
+        */
+        private void Add_Expenditure_Button_Click(object sender, RoutedEventArgs e)
+        {
+            adminSelectionHandler.showAddExpenseWindow();
+        }
+
+
+
+        /* 
+        *   
+        */
+        private void View_Expenses_Button_Click(object sender, RoutedEventArgs e)
+        {
+            adminSelectionHandler.showViewExpendituresWindow();
+        }
+
+
+
+        /* 
+        *   
+        */
+        private void View_User_Activity_Button_Click(object sender, RoutedEventArgs e)
+        {
+            adminSelectionHandler.showViewActivitiesWindow();
+        }
+
+
+
+        /* 
+        *   
+        */
+        private void Add_User_Button_Click(object sender, RoutedEventArgs e)
+        {
+            adminSelectionHandler.showAddUserWindow();
+        }
+    }
+}
